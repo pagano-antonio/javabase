c69cd5d383ac97fb465bf623c2460beb24022aa3
ANTONIO PAGANO <antonio.pagano@ANTONIOs-MacBook-Air.local>
2025-06-10 14:53:19 +0200

    hh

--- src/collection/mappe/TreeMapExample.java
+++ src/collection/mappe/TreeMapExample.java
@@ -2,24 +2,59 @@ package collection.mappe;
 
 import java.util.TreeMap;
 import java.util.Map;
+import java.util.Set;
 
 public class TreeMapExample {
     public static void main(String[] args) {
-//        Map<String, Integer> treeMap = new TreeMap<>();
-//
-//        // Adding elements
-//        treeMap.put("One", 1);
-//        treeMap.put("Two", 2);
-//        treeMap.put("Three", 3);
-//
-//        // Automatically sorted by key
-//        System.out.println("Sorted map:");
-//        for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
-//            System.out.println(entry.getKey() + " = " + entry.getValue());
-//        }
+    	
+    	/*
+      	//<chiave, valore>
+        Map< Integer, String> treeMap = new TreeMap<>();
+  
+         // Adding elements
+         treeMap.put( 150, "ciao");
+         treeMap.put(200, "viva");
+         treeMap.put(300, "hello");
+         
+   
+         String j = treeMap.get(200);
+         System.out.println(j);
+     */    
+        
+        //CHIAVE E VALORE possono essere dello stesso tipo
+        //Map<String, String> treeMapDUE = new TreeMap<>(); 
     	
     	
-    	
+    	//<chiave, valore>
+       Map<String, Integer> treeMap = new TreeMap<>();
+       
+        // Adding elements
+        treeMap.put("ciao", 150);
+        treeMap.put("viva", 200);
+        treeMap.put("hello", 300);
+        
+        // nNON SI PUO FARE
+  //     String f =  treeMap.get(23);
+  //     System.out.println(f);
+        
+        int j = treeMap.get("viva");
+        System.out.println(j);
+
+        System.out.println(treeMap);
+        
+        // Automatically sorted by key
+        Set<Map.Entry<String, Integer>> f = treeMap.entrySet();
+        
+        
+        System.out.println("Sorted map:");
+        for (Map.Entry<String, Integer> e : f) {
+        	
+            System.out.println(e.getKey() + " = " + e.getValue());
+        }
+        
+        
+  
+    /*	
         Map<Person, Integer> treeMap = new TreeMap<>();
 
         // Adding elements
@@ -32,6 +67,7 @@ public class TreeMapExample {
         for (Map.Entry<Person, Integer> entry : treeMap.entrySet()) {
             System.out.println(entry.getKey() + " = " + entry.getValue());
         }
+        */
     }
 }
 
